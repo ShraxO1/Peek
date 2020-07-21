4cb22c6b0a8255932047d9b726283dd38b210eb3
Shray Tyagi <shraytyagi1408@gmail.com>
2020-07-21 20:53:08 +0530

    first commit

--- images/forms.py
+++ images/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Image
-
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
@@ -9,6 +11,7 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput,
         }
+
     def clean_url(self):
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg']
@@ -16,3 +19,16 @@ class ImageCreateForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError('The given URL does not match valid extension')
         return url
+
+    def save(self, force_insert=False, force_update=False,commit=True):
+        image=super().save(commit=False)
+        image_url=self.cleaned_data['url']
+        name=slugify(image.title)
+        extension=image_url.rsplit('.',1)[1].lower()
+        image_name=f'{name}.{extension}'
+        response=request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()),
+                         save=False)
+        if commit:
+            image.save()
+        return image
