b03e09a6bfe190547e24bfce7e7f82943fa9f602
ShraxO1 <shraytyagi1408@gmail.com>
2020-10-26 16:02:59 +0530

    Final changes

--- images/forms.py
+++ images/forms.py
@@ -1,8 +1,9 @@
-from django import forms
-from .models import Image
 from urllib import request
+from django import forms
 from django.core.files.base import ContentFile
 from django.utils.text import slugify
+from .models import Image
+
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
@@ -15,19 +16,25 @@ class ImageCreateForm(forms.ModelForm):
     def clean_url(self):
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg']
-        extension = url.rsplit('.',1)[1].lower()
+        extension = url.rsplit('.', 1)[1].lower()
         if extension not in valid_extensions:
-            raise forms.ValidationError('The given URL does not match valid extension')
+            raise forms.ValidationError('The given URL does not ' \
+                                        'match valid image extensions.')
         return url
 
-    def save(self, force_insert=False, force_update=False,commit=True):
-        image=super().save(commit=False)
-        image_url=self.cleaned_data['url']
-        name=slugify(image.title)
-        extension=image_url.rsplit('.',1)[1].lower()
-        image_name=f'{name}.{extension}'
-        response=request.urlopen(image_url)
-        image.image.save(image_name, ContentFile(response.read()),
+    def save(self, force_insert=False,
+                   force_update=False,
+                   commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        # download image from the given URL
+        response = request.urlopen(image_url)
+        image.image.save(image_name,
+                         ContentFile(response.read()),
                          save=False)
         if commit:
             image.save()
--- people/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import People
-# Create your views here.
-
-def people_list(request):
-	obj = People.objects.all()
-	return render(request, 'people/people.html', {'obj':obj})
